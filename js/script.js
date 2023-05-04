32f77079384da1697129dcea24fe8745c28f995c
HarlingWilliam <williamharling@icloud.com>
2023-05-04 10:32:02 +0200

    v

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-// Display Mobile Menu
-const mobileMenu = () => {
+// Mobil meny
+function mobileMenu() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
-};
+}
 
 menu.addEventListener('click', mobileMenu);
